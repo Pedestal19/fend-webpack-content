d4d6dbca1501e30356b0e77b850a1277ddbcb1cc
Pedestal19 <gabeoji01@gmail.com>
2020-05-15 19:34:18 +0100

    tried creating cutom entry

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -2,5 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    
+    entry: '../src/client/index.js'
+    // entry: path.resolve(__dirname, 'src') + 'path/to/your/file.js',
 }
