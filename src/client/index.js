3e299b623f3f3f75c9ca4784aa0151dcdda6f6c7
Pedestal19 <gabeoji01@gmail.com>
2020-05-16 01:22:36 +0100

    clean up

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,7 @@ import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
 
 alert('I exist')
+alert('I exist')
+alert('I exist')
+
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,11 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     // entry: path.resolve(__dirname, 'src') + 'path/to/your/file.js',
     module: {
@@ -19,6 +22,12 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
